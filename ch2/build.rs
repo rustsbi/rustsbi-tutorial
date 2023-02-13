063c25267857bdb24fa2e06eae58151bb5d130a4
YdrMaster <ydrml@hotmail.com>
2023-02-13 18:13:09 +0800

    feat: 增加 linker 板块
    Signed-off-by: YdrMaster <ydrml@hotmail.com>

--- ch2/build.rs
+++ ch2/build.rs
@@ -1,41 +1,8 @@
 fn main() {
-    use std::{env, fs, path::PathBuf};
-
-    let ld = &PathBuf::from(env::var_os("OUT_DIR").unwrap()).join("linker.ld");
-    fs::write(ld, LINKER).unwrap();
     println!("cargo:rerun-if-changed=build.rs");
     println!("cargo:rerun-if-env-changed=LOG");
-    println!("cargo:rustc-link-arg=-T{}", ld.display());
+    println!(
+        "cargo:rustc-link-arg=-T{}",
+        linker::linker_script().display()
+    );
 }
-
-const LINKER: &[u8] = b"
-OUTPUT_ARCH(riscv)
-ENTRY(_start)
-MEMORY {
-    DRAM : ORIGIN = 0x80000000, LENGTH = 2M
-}
-SECTIONS {
-    .text : {
-        *(.text.entry)
-        *(.text .text.*)
-    } > DRAM
-    .rodata : {
-        *(.rodata .rodata.*)
-        *(.srodata .srodata.*)
-    } > DRAM
-    .data : {
-        *(.data .data.*)
-        *(.sdata .sdata.*)
-    } > DRAM
-    .bss (NOLOAD) : {
-        *(.bss.uninit)
-        sbss = .;
-        *(.bss .bss.*)
-        *(.sbss .sbss.*)
-        . = ALIGN(8);
-        ebss = .;
-    } > DRAM
-    /DISCARD/ : {
-        *(.eh_frame)
-    }
-}";
--- linker/src/lib.rs
+++ linker/src/lib.rs
@@ -1,13 +1,16 @@
-fn main() {
-    use std::{env, fs, path::PathBuf};
+//! 这个项目用于复用链接脚本文本和用于创建链接脚本文件的一小段代码。
 
-    let ld = &PathBuf::from(env::var_os("OUT_DIR").unwrap()).join("linker.ld");
-    fs::write(ld, LINKER).unwrap();
-    println!("cargo:rerun-if-changed=build.rs");
-    println!("cargo:rustc-link-arg=-T{}", ld.display());
+#![deny(warnings, missing_docs)]
+
+/// 创建链接脚本文件，返回文件路径。
+pub fn linker_script() -> std::path::PathBuf {
+    let ld = std::path::PathBuf::from(std::env::var_os("OUT_DIR").unwrap()).join("linker.ld");
+    std::fs::write(&ld, LINKER).unwrap();
+    ld
 }
 
-const LINKER: &[u8] = b"
+/// 链接脚本文本。
+pub const LINKER: &[u8] = b"
 OUTPUT_ARCH(riscv)
 ENTRY(_start)
 MEMORY {
